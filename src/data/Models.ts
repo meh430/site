dd7afc0d23e943464543331cc0ff766cc580b175
Mehul Pillai <mehulpillai@Mehuls-MacBook-Air.local>
2021-07-31 17:39:42 -0400

    First commit

---
+++ src/data/Models.ts
@@ -0,0 +1,40 @@
+
+export interface ImageItem {
+    image: string;
+}
+
+export interface Icon extends ImageItem {
+    isExternal: Boolean;
+    iconColor?: string;
+}
+  
+export interface ImageLink {
+    url: string;
+    icon: Icon;
+}
+
+export interface Profile extends ImageItem {
+    bio: string;
+    contactLinks: ImageLink[]
+}
+
+export interface Project {
+    projectName: string;
+    description: string;
+    projectLink: string;
+    technologies: string[];
+    features: string[];
+    images: string[]
+}
+
+export interface Skill {
+    skillName: string;
+    icon: Icon;
+}
+
+export interface Education extends ImageItem {
+    schoolName: string;
+    period: string;
+    details: string;
+}
+ 
