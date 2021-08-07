7967c1b64399c213caaff386cdf3c26de1c8b973
Mehul Pillai <mehulpillai@Mehuls-MacBook-Air.local>
2021-08-07 18:28:01 -0400

    Adds projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,37 @@
-import { PropsItem } from "../data/Models"
+import { Card, useTheme } from "@material-ui/core";
+import { Project, PropsItem } from "../data/Models";
+import { getCardStyle } from "./Profile";
 
 export const Projects = (props: PropsItem) => {
-  return <h1>Projects</h1>
-}
+  const projects = props.dataRepo.getProjects();
+  return (
+    <div className="row" style={{justifyContent: "center", marginTop: "10px"}}>
+      { projects.map(project => <ProjectCard {...project}/>) }
+    </div>
+  )
+};
+
+const ProjectCard = (project: Project) => {
+  const theme = useTheme();
+  const cardStyle = {
+    ...getCardStyle(theme, 11),
+    padding: "20px",
+    margin: "10px",
+    width: "90%",
+    maxWidth: "250px",
+    alignItems: "center",
+  };
+  const textStyle = { margin: "0px", padding: "0px", marginBottom: "10px" };
+  const iconStyle = { margin: "10px", fontSize: "40px", color: theme.palette.text.primary }
+  return (
+    <Card className="col" style={cardStyle}>
+      <h4 style={textStyle}>{project.projectName}</h4>
+      <h5 style={textStyle}>{project.technologies}</h5>
+      <p style={textStyle}>{project.description}</p>
+      <div className="row">
+        <i className="fab fa-github" style={iconStyle} />
+        <i className="fas fa-info-circle" style={iconStyle} />
+      </div>
+    </Card>
+  );
+};
